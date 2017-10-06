6878c618f2ecbb9910c4c696439c40716c61fb9c
davidnenu <keroberos1985@gmail.com>
2017-10-06 12:00:58 +0200

    4/10/2017

--- app/src/main/java/com/example/dm2/ejerciciobotonesyimagenes/Ej6.java
+++ app/src/main/java/com/example/dm2/ejerciciobotonesyimagenes/Ej6.java
@@ -11,18 +11,60 @@ public class Ej6 extends AppCompatActivity {
     ToggleButton tb;
     Switch sw;
     ImageView img;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ej6);
     }
 
-    public void mostrar(View v){
+    public void mostrarSwitch(View v) {
+        sw = (Switch) findViewById(v.getId());
+        if (sw.getId() == R.id.switch1) {
+            if (sw.isChecked()) {
+                img = (ImageView) findViewById(R.id.camara1);
+                img.setVisibility(View.VISIBLE);
+            } else {
+                img = (ImageView) findViewById(R.id.camara1);
+                img.setVisibility(View.INVISIBLE);
+            }
+        } if (sw.getId() == R.id.switch2) {
+            if (sw.isChecked()) {
+                img = (ImageView) findViewById(R.id.luz1);
+                img.setImageResource(R.drawable.luzon);
+            } else {
+                img = (ImageView) findViewById(R.id.luz1);
+                img.setImageResource(R.drawable.luz);
+            }
 
-        if(findViewById(v.getId()).toString().equals("toggle1")){
-            img=(ImageView)findViewById(R.id.camara1);
-            img.setImageResource(R.drawable.camara);
         }
 
     }
-}
+
+
+
+    public void mostrarToggle(View v){
+        tb = (ToggleButton) findViewById(v.getId());
+        if (tb.getId() == R.id.toggle1) {
+            if (tb.isChecked()) {
+                img = (ImageView) findViewById(R.id.camara2);
+                img.setVisibility(View.VISIBLE);
+            } else {
+                img = (ImageView) findViewById(R.id.camara2);
+                img.setVisibility(View.INVISIBLE);
+            }
+        } if (tb.getId() == R.id.toggle2) {
+            if (tb.isChecked()) {
+                img = (ImageView) findViewById(R.id.luz2);
+                img.setImageResource(R.drawable.luzon);
+            } else {
+                img = (ImageView) findViewById(R.id.luz2);
+                img.setImageResource(R.drawable.luz);
+            }
+
+        }
+
+    }
+
+    }
+
